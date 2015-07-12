13de829ed61d838f556759660a39106de6467bce
andrzejewski <adix.adrian@gmail.com>
2015-07-12 15:49:34 +0200

    Removed margin and padding helpers, updated docs

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,25 @@
 
   var appSources = 'app/**/*.*',
       dist = './dist/',
+      excludeMqSources = '!app/media-queries/_mq.scss',
 
       gulp = require('gulp'),
       $ = require('gulp-load-plugins')(),
-      runSequence = require('run-sequence'),
       bump = require('gulp-bump'),
       gutil = require('gulp-util'),
       git = require('gulp-git'),
       fs = require('fs'),
       del = require('del'),
-      sassdoc = require('sassdoc');
+      sassdoc = require('sassdoc'),
+      args = require('minimist')(process.argv.slice(2));
 
   gulp.task('doc', function () {
     var options = {
       dest: 'doc',
       verbose: true,
     };
-    return gulp.src(appSources)
-    .pipe(sassdoc());
+    return gulp.src([appSources, excludeMqSources])
+    .pipe(sassdoc(options));
   });
 
   gulp.task('clean', function (cb) {
@@ -35,12 +36,14 @@
   gulp.task('dist', ['copy-app']);
 
   gulp.task('bump-version', ['dist'], function () {
+    var type = (args.type === undefined) ? 'patch' : args.type;
+
     return gulp.src(['./bower.json', './package.json'])
-      .pipe(bump({type: "patch"}).on('error', gutil.log))
+      .pipe(bump({type: type}).on('error', gutil.log))
       .pipe(gulp.dest('./'));
   });
 
-  gulp.task('commit-changes', ['bump-version'], function () {
+  gulp.task('commit-changes', ['doc', 'bump-version'], function () {
     return gulp.src('.')
       .pipe(git.commit('[Prerelease] Bumped version number', {args: '-a'}));
   });
@@ -63,6 +66,7 @@
     };
   });
 
+  //MAJOR.MINOR.PATCH
   gulp.task('release', ['create-new-tag']);
 
 })();
